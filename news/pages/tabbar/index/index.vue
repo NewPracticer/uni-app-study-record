27cf7da15c0e22f225e3363ea9285eea16c0c218
tianqing.liang <1>
2022-04-06 17:31:10 +0800

    '自定义导航栏,云原生'

---
+++ news/pages/tabbar/index/index.vue
@@ -0,0 +1,54 @@
+<template>
+	<view class="content">
+		<navbar></navbar>
+		<view v-for="item in 100">{{item}} 内容那个</view>
+	</view>
+</template>
+
+<script>
+	import navbar from '@/components/navbar/navbar.vue'
+	export default {
+		components:{
+			navbar
+		},
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
---
+++ news/unpackage/dist/dev/mp-weixin/components/navbar.js
@@ -0,0 +1,24 @@
+(global["webpackJsonp"] = global["webpackJsonp"] || []).push([["components/navbar"],{
+
+/***/ 32:
+/*!*********************************************************************!*\
+  !*** E:/gitproject/uni-app-study-record/news/components/navbar.vue ***!
+  \*********************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/@dcloudio/webpack-uni-mp-loader/lib/style.js):\nError: ENOENT: no such file or directory, open 'E:\\gitproject\\uni-app-study-record\\news\\components\\navbar.vue'");
+
+/***/ })
+
+}]);
+//# sourceMappingURL=../../.sourcemap/mp-weixin/components/navbar.js.map
+;(global["webpackJsonp"] = global["webpackJsonp"] || []).push([
+    'components/navbar-create-component',
+    {
+        'components/navbar-create-component':(function(module, exports, __webpack_require__){
+            __webpack_require__('1')['createComponent'](__webpack_require__(32))
+        })
+    },
+    [['components/navbar-create-component']]
+]);
